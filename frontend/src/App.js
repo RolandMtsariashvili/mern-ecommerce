6a54759b6b73e49c052bcb2a3f4baefb4ae279e9
Roland Mtsariashvili <rolandi.mtsariashvili@btu.edu.ge>
2021-04-16 20:44:47 +0400

    feat: move template's styling to react app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,54 @@
 import React from 'react';
+import data from './data';
 
 function App() {
   return (
-    <div className="App" />
+    <div className="grid-container">
+      <header className="header">
+        <div className="header__wrapper">
+          <div>
+            <a href="/" className="header__logo">
+              MERNStore
+            </a>
+          </div>
+          <div className="header__right">
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a>
+          </div>
+        </div>
+      </header>
+      <main className="main">
+        <div className="container">
+          <div className="products">
+            {data.products.map((product) => (
+              <div className="product-card" key={product.id}>
+                <a href={`/product/${product.id}`}>
+                  <img
+                    className="product-card__img"
+                    src={product.image}
+                    alt={product.description}
+                  />
+                </a>
+                <a href={`/product/${product.id}`}>
+                  <h2>{product.name}</h2>
+                </a>
+                <div className="rating">
+                  <span><i className="fa fa-star" /></span>
+                  <span><i className="fa fa-star" /></span>
+                  <span><i className="fa fa-star" /></span>
+                  <span><i className="fa fa-star" /></span>
+                  <span><i className="fa fa-star" /></span>
+                </div>
+                <div className="price">{`$${product.price}`}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </main>
+      <footer className="footer">
+        <p>All Rights Reserved</p>
+      </footer>
+    </div>
   );
 }
 
