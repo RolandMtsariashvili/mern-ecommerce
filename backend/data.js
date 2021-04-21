e7ad4c637b65cd5f9928d2be88797337f335e69f
Roland Mtsariashvili <rolandi.mtsariashvili@btu.edu.ge>
2021-04-22 01:34:37 +0400

    feat: add basic server configuration

---
+++ backend/data.js
@@ -0,0 +1,47 @@
+const data = {
+  products: [
+    {
+      id: '1',
+      name: 'Nike\'s Coat',
+      category: 'Coats',
+      image: 'https://via.placeholder.com/250x350',
+      price: 110,
+      brand: 'Nike',
+      rating: 4.5,
+      reviewsNum: 17,
+      description: 'High Quality Nike Coat',
+    },
+    {
+      id: '2',
+      name: 'Nike Slim trousers',
+      image: 'https://via.placeholder.com/250x350',
+      price: 78,
+      brand: 'Nike',
+      rating: 3.8,
+      reviewsNum: 8,
+      description: 'Brand New Slim Trousers',
+    },
+    {
+      id: '3',
+      name: 'Zara\'s Shirt',
+      image: 'https://via.placeholder.com/250x350',
+      price: 178,
+      brand: 'Zara',
+      rating: 4.8,
+      reviewsNum: 21,
+      description: 'Zara\'s skinny shirt',
+    },
+    {
+      id: '4',
+      name: 'H&M Baseball Hat',
+      image: 'https://via.placeholder.com/250x350',
+      price: 28,
+      brand: 'H&M',
+      rating: 4.0,
+      reviewsNum: 14,
+      description: 'Hood Quality Hat For Baseball Lovers',
+    },
+  ],
+};
+
+export default data;
