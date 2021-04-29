6693511a226d028d33a3ba8b78c7df9c80041f69
Roland Mtsariashvili <rolandi.mtsariashvili@btu.edu.ge>
2021-04-29 17:10:39 +0400

    feat: add sample users seeding

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+  users: [
+    {
+      name: 'Adming',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('1234', 8),
+      isAdmin: true,
+    },
+    {
+      name: 'John',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('1234', 8),
+      isAdmin: false,
+    }
+  ],
   products: [
     {
       id: '1',
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import data from './data.js';
+import userRouter from './routers/userRouter.js';
 
 const app = express();
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/mernStore', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 
 app.get('/', (req, res) => {
   res.send('Server Started Working');
@@ -16,10 +23,17 @@ app.get('/api/products/:id', (req, res) => {
   }
 });
 
+app.use('/api/users', userRouter);
+
 app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
 
+app.use((err, req, res, next) => {
+  res.status(500).send({
+    message: err.message,
+  })
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
